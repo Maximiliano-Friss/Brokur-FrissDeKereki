0d4cd8a9a9b96d0c5d7ea86f7f9a2af0d062ac89
Maximiliano Friss <mfrissdekereki@gmail.com>
2022-07-08 18:51:55 -0300

    Se agrega ItemCount a componente ItemDetail

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,4 +1,11 @@
+import ItemCount from "../ItemListContainer/ItemCount"
+
 const ItemDetail = ({item}) => {
+    
+    const onAdd = () => {
+        alert('Tus productos se agregaron al carrito con éxito')
+    }
+    
     return (
         <div className="grid grid-cols-2 gap-5">
             <img className="w-50" src={item.pictureURL} alt={item.title} />
@@ -6,6 +13,7 @@ const ItemDetail = ({item}) => {
                 <h2 id='h2-title'>{item.title}</h2>
                 <h3 id="h3-price">${item.price}</h3>
                 <h4 id='h4-description'>{item.description}</h4>
+                <ItemCount initial={0} stock={5} onAdd={onAdd} />
             </div>
         </div>
     )
