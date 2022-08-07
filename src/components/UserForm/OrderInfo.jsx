fd6e3d1372a312c4c8fa4edb7cb3ed051fb283f5
Maximiliano Friss <mfrissdekereki@gmail.com>
2022-08-07 20:55:01 -0300

    Se instala react form hooks

---
+++ src/components/UserForm/OrderInfo.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useContext } from 'react';
+import { contextoCart } from '../../context/CartContext'
+
+const OrderInfo = ({idSale, total}) => {
+    const {clear} = useContext(contextoCart);
+    
+    useEffect(() => {
+        clear();
+    })
+
+    return (
+        <div className="order-info-container">
+            <h2>Thank you for your purchase!</h2>
+            <h3 className="mt-5">Here is some relevant information about your order.</h3>
+            <p><span className="font-bold">ID ORDER NUMBER:</span> {idSale}</p>
+            <p><span className="font-bold">TOTAL:</span> {total}</p>
+        </div>
+    )
+}
+
+export default OrderInfo 
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,18 +1,19 @@
 import { useContext, useState} from "react"
 import { contextoCart } from '../../context/CartContext'
+import OrderInfo from './OrderInfo'
 import {db} from '../../firebase/firebase'
 import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
 
 const UserForm = () => {
     const {products} = useContext(contextoCart);
     const {total} = useContext(contextoCart);
-    const {clear} = useContext(contextoCart);
     const [cardName, setCardName] = useState('');
     const [cardNumber, setCardNumber] = useState('');
     const [cardExpiration, setCardExpiration] = useState('');
     const [cardCVV, setCardCVV] = useState('');
     const [purchaseConfirmed, setPurchaseConfirmed] = useState(false);
-    const [idVenta, setIdVenta] = useState('');
+    const [idSale, setIdSale] = useState('');
+    const [saleTotal, setSaleTotal] = useState('');
     
     const getCardName = (event) => {
         setCardName(event.target.value);
@@ -39,22 +40,16 @@ const UserForm = () => {
             total: total,
         })
         .then((result) => {
-            setIdVenta(result.id);
+            setIdSale(result.id);
+            setSaleTotal(total)
         })
         setPurchaseConfirmed(true);
-        clear();
     }
 
     return (
             <div className='mx-auto text-center'>
                 {purchaseConfirmed ?
-                <div className="order-info-container">
-                    <h2>Thank you for your purchase!</h2>
-                    <h3 className="mt-5">Here is some relevant information about your order.</h3>
-                    <p>
-                        <span className="font-bold">ID ORDER NUMBER:</span> {idVenta}
-                    </p>
-                </div>
+                <OrderInfo idSale={idSale} total={saleTotal} products={products}/>
                 :
                 <>
                     <h2>Please, fill the blanks with your card information.</h2>
@@ -62,7 +57,7 @@ const UserForm = () => {
                         <label htmlFor="cardName">Name on card:</label>
                         <input value={cardName} type="text" required onChange={getCardName} id="cardName" name="cardName"/>
                         <label htmlFor="cardNumber">Card number:</label>
-                        <input value={cardNumber} type="tel" inputmode="numeric" pattern="[0-9]" maxlength="16" placeholder="xxxx xxxx xxxx xxxx" onChange={getCardNumber} id="cardNumber" name="cardNumber"/>
+                        <input value={cardNumber} type="tel" inputMode="numeric" pattern="[0-9]" maxLength="16" placeholder="xxxx xxxx xxxx xxxx" onChange={getCardNumber} id="cardNumber" name="cardNumber"/>
                         <div className="flex flex-row mt-5 justify-evenly">
                             <div className="w-1/2 flex flex-col form-credit mx-1">
                                 <label htmlFor="CardExpiration">Expiration Date:</label>
@@ -70,7 +65,7 @@ const UserForm = () => {
                             </div>
                             <div className="w-1/2 flex flex-col form-credit mx-1">
                                 <label htmlFor="cardCVV">CVV:</label>
-                                <input value={cardCVV} onChange={getCardCVV} id="cardCVV" name="cardCVV" type="tel" inputmode="numeric" pattern="[0-9]" maxlength="3" placeholder="xxx"/>
+                                <input value={cardCVV} onChange={getCardCVV} id="cardCVV" name="cardCVV" type="tel" inputMode="numeric" pattern="[0-9]" maxLength="3" placeholder="xxx"/>
                             </div>
                         </div>
                         <submit className='form-submit' onClick={endPurchase}>Confirm</submit>
