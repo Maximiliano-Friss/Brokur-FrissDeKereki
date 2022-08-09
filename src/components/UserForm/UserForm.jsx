b7b212c0afe148d69bc7e6ff66d4d99e18a5c8c3
Maximiliano Friss <mfrissdekereki@gmail.com>
2022-08-08 21:39:21 -0300

    Se modifica form

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -3,48 +3,32 @@ import { contextoCart } from '../../context/CartContext'
 import OrderInfo from './OrderInfo'
 import {db} from '../../firebase/firebase'
 import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
+import { useForm } from "react-hook-form";
 
 const UserForm = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const {products} = useContext(contextoCart);
     const {total} = useContext(contextoCart);
-    const [cardName, setCardName] = useState('');
-    const [cardNumber, setCardNumber] = useState('');
-    const [cardExpiration, setCardExpiration] = useState('');
-    const [cardCVV, setCardCVV] = useState('');
     const [purchaseConfirmed, setPurchaseConfirmed] = useState(false);
     const [idSale, setIdSale] = useState('');
     const [saleTotal, setSaleTotal] = useState('');
-    
-    const getCardName = (event) => {
-        setCardName(event.target.value);
-    }
-    const getCardNumber = (event) => {
-        setCardNumber(event.target.value);
-    }
-    const getCardExpiration = (event) => {
-        setCardExpiration(event.target.value);
-    }
-    const getCardCVV = (event) => {
-        setCardCVV(event.target.value);
+    const saleInfo = {
+        items: products,
+        date: serverTimestamp(),
+        total: total,
     }
 
-    const endPurchase = () => {
+    const onSubmit = (cardInfo) => {
         const salesCollection = collection(db, 'sales');
-        addDoc(salesCollection, {
-            cardName: cardName,
-            cardNumber: cardNumber,
-            cardExpiration: cardExpiration,
-            cardCVV: cardCVV,
-            items: products,
-            date: serverTimestamp(),
-            total: total,
-        })
+        addDoc(salesCollection, {...saleInfo, cardInfo})
         .then((result) => {
             setIdSale(result.id);
             setSaleTotal(total)
         })
         setPurchaseConfirmed(true);
     }
+
+
 
     return (
             <div className='mx-auto text-center'>
@@ -53,22 +37,27 @@ const UserForm = () => {
                 :
                 <>
                     <h2>Please, fill the blanks with your card information.</h2>
-                    <form className="flex flex-col">
-                        <label htmlFor="cardName">Name on card:</label>
-                        <input value={cardName} type="text" required onChange={getCardName} id="cardName" name="cardName"/>
-                        <label htmlFor="cardNumber">Card number:</label>
-                        <input value={cardNumber} type="tel" inputMode="numeric" pattern="[0-9]" maxLength="16" placeholder="xxxx xxxx xxxx xxxx" onChange={getCardNumber} id="cardNumber" name="cardNumber"/>
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+                        <label>Name on card:</label>
+                        <input type='text' {...register("cardName", {required: true})} />
+                        {errors.cardName?.type === 'required' && "Please enter the name as shown on the card"}
+                        {/* value={cardName} type="text" required onChange={getCardName} id="cardName" name="cardName" */}
+                        <label>Card number:</label>
+                        <input type="number" placeholder="XXXX XXXX XXXX XXXX"  {...register("cardNumber", {required: true, valueAsNumber: true, maxLength: 16})} />
+                        {/* <input value={cardNumber} type="tel" inputMode="numeric" pattern="[0-9]" maxLength="16" placeholder="xxxx xxxx xxxx xxxx" onChange={getCardNumber} id="cardNumber" name="cardNumber"/> */}
                         <div className="flex flex-row mt-5 justify-evenly">
                             <div className="w-1/2 flex flex-col form-credit mx-1">
-                                <label htmlFor="CardExpiration">Expiration Date:</label>
-                                <input value={cardExpiration} type="month" onChange={getCardExpiration} id="CardExpiration" name="CardExpiration" min="2022-08"/>
+                                <label>Expiration Date:</label>
+                                <input type="month" {...register("cardExpiration", {required: true, valueAsDate: true})} />
+                                {/* <input value={cardExpiration} type="month" onChange={getCardExpiration} id="CardExpiration" name="CardExpiration" min="2022-08"/> */}
                             </div>
                             <div className="w-1/2 flex flex-col form-credit mx-1">
-                                <label htmlFor="cardCVV">CVV:</label>
-                                <input value={cardCVV} onChange={getCardCVV} id="cardCVV" name="cardCVV" type="tel" inputMode="numeric" pattern="[0-9]" maxLength="3" placeholder="xxx"/>
+                                <label>CVV:</label>
+                                <input type="number" placeholder="XXX" {...register("cardCVV", {required: true, valueAsNumber: true, minLength:3, maxLength: 3})} />
+                                {/* <input value={cardCVV} onChange={getCardCVV} id="cardCVV" name="cardCVV" type="tel" inputMode="numeric" pattern="[0-9]" maxLength="3" placeholder="xxx"/> */}
                             </div>
                         </div>
-                        <submit className='form-submit' onClick={endPurchase}>Confirm</submit>
+                        <input type='submit' className='form-submit' value='Confirm'></input>
                     </form>
                 </>
                 }
